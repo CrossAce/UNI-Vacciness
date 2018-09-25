e4f5979b94f44f222e3a7f1f89f3a93af1e5fb71
Vick Enev <acexdark@gmail.com>
2018-09-25 12:43:41 +0300

    Vacciness

---
+++ ST/ST/Form1.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace ST
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.uname = new System.Windows.Forms.TextBox();
+            this.pword = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(37, 64);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(58, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Username:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(37, 123);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Password:";
+            // 
+            // uname
+            // 
+            this.uname.Location = new System.Drawing.Point(103, 61);
+            this.uname.MaxLength = 10;
+            this.uname.Name = "uname";
+            this.uname.Size = new System.Drawing.Size(152, 20);
+            this.uname.TabIndex = 2;
+            // 
+            // pword
+            // 
+            this.pword.Location = new System.Drawing.Point(103, 116);
+            this.pword.MaxLength = 8;
+            this.pword.Name = "pword";
+            this.pword.Size = new System.Drawing.Size(152, 20);
+            this.pword.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(121, 164);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "OK";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(343, 199);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pword);
+            this.Controls.Add(this.uname);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Log In";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox uname;
+        private System.Windows.Forms.TextBox pword;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ ST/ST/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+
+namespace ST
+{
+    public partial class Form1 : Form
+    {
+
+        const string _patternUname = @"^[^1234567890][a-z1-5A-Z]+";
+       
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            bool hasError = true;
+            if(uname.Text.Length > 4 && pword.Text.Length > 5)
+            {
+                hasError = !(ValidateRegex(_patternUname,uname.Text) && pword.Text.Length < 9);
+            }
+            if (hasError)
+                MessageBox.Show("Error in credentials!","Error",MessageBoxButtons.OKCancel, MessageBoxIcon.Error);
+            else
+            {
+                LogIn();
+            }
+        }
+
+        private bool ValidateRegex(string pattern, string text)
+        {
+            
+            Regex reg = new Regex(pattern);
+            return reg.IsMatch(text);
+        }
+
+        private void LogIn()
+        {
+
+        }
+    }
+}
